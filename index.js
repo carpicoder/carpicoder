458db8bc77b5f8b393c2b54882ddf04dffbd4285
carpicoder <webcarpi@gmail.com>
2024-05-11 22:13:01 +0200

    test api key

--- index.js
+++ index.js
@@ -18,18 +18,21 @@ let DATA = {
 };
 
 async function generateReadMe() {
-  const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC53KeIgcYPozO6SqlN6edbw&key=${apiKey}`);
-  const data = await response.json();
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC53KeIgcYPozO6SqlN6edbw&key=${apiKey}`);
+    const data = await response.json();
 
-  const subscriberCount = data.items[0].statistics.subscriberCount;
+    if (data.items && data.items.length > 0) {
+        const subscriberCount = data.items[0].statistics.subscriberCount;
+        DATA.subscriberCount = subscriberCount;
+    } else {
+        console.error("No se encontraron datos de estadísticas en la respuesta.");
+    }
 
-  DATA.subscriberCount = subscriberCount;
-
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        const output = Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+    });
 }
 
 generateReadMe();
